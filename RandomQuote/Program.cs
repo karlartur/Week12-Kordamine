5cc81392ce0e2f10a3d60f06c964b2e69458c393
karlartur <89841872+karlartur@users.noreply.github.com>
2021-11-17 10:16:32 +0200

    Add files via upload

---
+++ RandomQuote/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+namespace RandomQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\Sample\RandomQuote.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            Console.WriteLine(dataFromFile[0]);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, dataFromFile.Length);
+            Console.WriteLine(dataFromFile[randomIndex]);
+        }
+        
+    }
+}
