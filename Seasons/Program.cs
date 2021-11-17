6c91f57cf28054e3325ba1312ab5efac36411bf5
karlartur <89841872+karlartur@users.noreply.github.com>
2021-11-17 09:51:51 +0200

    Add files via upload

---
+++ Seasons/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace Seasons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string autumnFile = @"C:\Users\opilane\Sample\autumn.txt";
+            string winterFile = @"C:\Users\opilane\Sample\winter.txt";
+
+            string[] winterData = File.ReadAllLines(autumnFile);
+            string[] autumnData = File.ReadAllLines(winterFile);
+
+            File.WriteAllLines(autumnFile, autumnData);
+            File.WriteAllLines(winterFile, winterData);
+
+
+
+
+
+
+
+
+        }
+    }
+}
